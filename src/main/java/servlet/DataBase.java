9bba33bfce3688aca458b49b520178769b9ca8fd
ariG23498 <aritra.born2fly@gmail.com>
2019-11-15 12:06:05 +0530

    cheanges in db

--- src/main/java/servlet/DataBase.java
+++ src/main/java/servlet/DataBase.java
@@ -9,7 +9,7 @@ import javax.xml.crypto.Data;
 public class DataBase {
    // JDBC driver name and database URL
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-   static final String DB_URL = "jdbc:mysql://localhost:3306/sayak";
+   static final String DB_URL = "jdbc:mysql://10.96.0.1:3306/inframind";
 
    // Database credentials
    static final String USER = "aritra";
